ca1248325fb0b7db0628173c2b4a89bb05783aca
Alberto Aldegheri <albyrock87@gmail.com>
2024-02-02 12:30:35 +0100

    Cleanup and prepare for docs site

---
+++ Tests/Nalu.Maui.Test/NSubstituteLeakTests.cs
@@ -0,0 +1,39 @@
+namespace Nalu.Maui.Test;
+
+public class NSubstituteLeakTests
+{
+    public class Foo;
+
+    public interface IInterface
+    {
+        public void Method(Foo foo);
+    }
+
+    // private readonly IInterface _interfaceMock = Substitute.For<IInterface>();
+    [Fact(DisplayName = "NSubstitute does not leak")]
+    public async Task NSubstituteDoesNotLeak()
+    {
+        WeakReference<Foo> weakFoo;
+        {
+            var foo = new Foo();
+            weakFoo = new WeakReference<Foo>(foo);
+
+            // _interfaceMock.Method(foo);
+            // _interfaceMock.ClearReceivedCalls();
+        }
+
+        await GcCollect();
+
+        weakFoo.TryGetTarget(out _).Should().BeFalse();
+    }
+
+    private static async Task GcCollect()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            await Task.Yield();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
