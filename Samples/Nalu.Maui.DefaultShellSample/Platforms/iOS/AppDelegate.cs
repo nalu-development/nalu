5759a7b0bf3aa73163f54e189b16bf85ee076ea8
Alberto Aldegheri <albyrock87@gmail.com>
2024-09-24 17:18:09 +0200

    Chore: Delete default shell sample

--- Samples/Nalu.Maui.DefaultShellSample/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nalu.Maui.DefaultShellSample;
-
-using Foundation;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
