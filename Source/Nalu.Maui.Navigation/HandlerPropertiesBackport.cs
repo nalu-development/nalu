d1f96681c45fb7f10bb3577e802a91dbd4805f5a
Alberto Aldegheri <albyrock87@gmail.com>
2024-12-04 15:46:19 +0100

    Add automatic handler disconnection when leaving the page

---
+++ Source/Nalu.Maui.Navigation/HandlerPropertiesBackport.cs
@@ -0,0 +1,22 @@
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Nalu;
+
+using System.ComponentModel;
+
+[Obsolete("This is a .NET9 backport and it will be removed in version 9.0.0 where it has no effect.")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class HandlerPropertiesBackport
+{
+    public static readonly BindableProperty DisconnectPolicyBackportProperty = BindableProperty.CreateAttached(
+        "DisconnectPolicyBackport",
+        typeof(HandlerDisconnectPolicyBackport),
+        typeof(HandlerPropertiesBackport),
+        HandlerDisconnectPolicyBackport.Automatic);
+
+    public static void SetDisconnectPolicyBackport(BindableObject target, HandlerDisconnectPolicyBackport value)
+        => target.SetValue(DisconnectPolicyBackportProperty, value);
+
+    public static HandlerDisconnectPolicyBackport GetDisconnectPolicyBackport(BindableObject target)
+        => (HandlerDisconnectPolicyBackport)target.GetValue(DisconnectPolicyBackportProperty);
+}
