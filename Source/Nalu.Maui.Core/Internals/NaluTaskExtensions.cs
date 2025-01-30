9670f5f8d211f22ca9ee6c4f0474bf925771269a
Alberto Aldegheri <albyrock87@gmail.com>
2025-01-30 15:19:22 +0100

    Fix android back button crashing the app when pressed very fast

---
+++ Source/Nalu.Maui.Core/Internals/NaluTaskExtensions.cs
@@ -0,0 +1,56 @@
+namespace Nalu.Internals;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+[SuppressMessage("Usage", "VSTHRD003:Avoid awaiting foreign Tasks", Justification = "Proven safe in MAUI")]
+[SuppressMessage("Usage", "VSTHRD100:Avoid async void methods", Justification = "Proven safe in MAUI")]
+internal static class NaluTaskExtensions
+{
+    public static async void FireAndForget<TResult>(this Task<TResult> task, Action<Exception>? errorCallback = null)
+    {
+        TResult? result = default;
+        try
+        {
+            result = await task.ConfigureAwait(false);
+        }
+        catch (Exception exc)
+        {
+            errorCallback?.Invoke(exc);
+#if DEBUG
+            throw;
+#endif
+        }
+    }
+
+    public static async void FireAndForget(this Task task, Action<Exception>? errorCallback = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            errorCallback?.Invoke(ex);
+#if DEBUG
+            throw;
+#endif
+        }
+    }
+
+    public static void FireAndForget(this Task task, ILogger? logger, [CallerMemberName] string? callerName = null) =>
+        task.FireAndForget(ex => Log(logger, ex, callerName));
+
+    public static void FireAndForget<T>(this Task task, T? viewHandler, [CallerMemberName] string? callerName = null)
+        where T : IElementHandler =>
+        task.FireAndForget(ex => Log(viewHandler?.CreateLogger<T>(), ex, callerName));
+
+    private static ILogger? CreateLogger<T>(this IElementHandler? elementHandler) =>
+        elementHandler?.MauiContext?.Services?.GetService<ILogger<T>>();
+
+#pragma warning disable CA1848
+    private static void Log(ILogger? logger, Exception ex, string? callerName) =>
+        logger?.LogError(ex, "Unexpected exception in {Member}.", callerName);
+#pragma warning restore CA1848
+}
