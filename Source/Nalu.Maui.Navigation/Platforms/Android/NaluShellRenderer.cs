1624f550016b9d4d60ed3acb51817691d8c1e6c2
Alberto Aldegheri <albyrock87@gmail.com>
2025-11-24 14:59:16 +0100

    Android renderer

--- Source/Nalu.Maui.Navigation/Platforms/Android/NaluShellRenderer.cs
+++ Source/Nalu.Maui.Navigation/Platforms/Android/NaluShellRenderer.cs
@@ -1,102 +1,15 @@
 #nullable enable
-using Android.Graphics;
-using Android.Graphics.Drawables;
-using Android.Widget;
-using Google.Android.Material.BottomNavigation;
-using Google.Android.Material.Internal;
-using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
-using Microsoft.Maui.Platform;
-using Color = Android.Graphics.Color;
-using Font = Microsoft.Maui.Font;
-using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
-using View = Android.Views.View;
 
 namespace Nalu;
 
 public class NaluShellRenderer : ShellRenderer
 {
-    protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
-        => new NaluShellBottomNavViewAppearanceTracker(this, ((IPlatformViewHandler)this).MauiContext ?? throw new InvalidOperationException("MauiContext should be set at this point."), shellItem);
-
-    protected override IShellToolbarTracker CreateTrackerForToolbar(Toolbar toolbar)
+    protected override IShellItemRenderer CreateShellItemRenderer(ShellItem shellItem)
     {
-        // https://github.com/dotnet/maui/issues/7045
-        var shellToolbarTracker = base.CreateTrackerForToolbar(toolbar);
-        shellToolbarTracker.TintColor = Colors.Black;
-
-        return shellToolbarTracker;
-    }
-}
-
-file class NaluShellBottomNavViewAppearanceTracker : ShellBottomNavViewAppearanceTracker
-{
-    private readonly IShellContext _shellContext;
-    private readonly IMauiContext _mauiContext;
-    private readonly ShellItem _shellItem;
-    private View? _nativeMauiBar;
-
-    public NaluShellBottomNavViewAppearanceTracker(IShellContext shellContext, IMauiContext mauiContext, ShellItem shellItem)
-        : base(shellContext, shellItem)
-    {
-        _shellContext = shellContext;
-        _mauiContext = mauiContext;
-        _shellItem = shellItem;
-    }
-
-    public override void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
-    {
-        base.SetAppearance(bottomView, appearance);
-
-        bottomView.Elevation = bottomView.Context!.ToPixels(8); // this should work with 2dp, but the shadow is almost invisible
-        bottomView.Background = new ColorDrawable(Color.White); // background must be set for the shadow to be visible
-        
-        // Get the TabBarView from the ShellItem
-        var tabBarView = NaluShell.GetTabBarView(_shellItem);
-        
-        // Use the provided view or fallback to a default red Grid
-        Microsoft.Maui.Controls.View mauiBar = tabBarView ?? new Grid
-        {
-            BackgroundColor = Colors.Red
-        };
-        
-        if (_nativeMauiBar == null)
-        {
-            var platformView = mauiBar.ToPlatform(_mauiContext);
-            _nativeMauiBar = platformView;
-            _nativeMauiBar.TranslationZ = 1;
-            _nativeMauiBar.Clickable = true;
-            _nativeMauiBar.Focusable = true;
-            bottomView.AddView(_nativeMauiBar);
-        }
-
-#pragma warning disable XAOBS001
-        if (bottomView.GetChildAt(0) is not BottomNavigationMenuView bottomNavMenuView)
-        {
-            return;
-        }
-
-        var fontManager = _shellContext.Shell.Handler!.GetRequiredService<IFontManager>();
-        var font = Font.OfSize("InterRegular", 12);
-        var fontSelected = Font.OfSize("InterSemiBold", 12);
-        var fontFace = fontManager.GetTypeface(font);
-        var fontFaceSelected = fontManager.GetTypeface(fontSelected);
-
-        // Thankfully items are already available at this point
-        // So we can loop through them and set the typeface
-        for (var i = 0; i < bottomNavMenuView.ChildCount; i++)
-        {
-            var item = (BottomNavigationItemView) bottomNavMenuView.GetChildAt(i)!;
-            var itemTitle = item.GetChildAt(1);
-
-            var baselineLayout = itemTitle as BaselineLayout;
-            var smallTextView = (TextView?) baselineLayout?.GetChildAt(0);
-            var largeTextView = (TextView?) baselineLayout?.GetChildAt(1);
-
-            smallTextView?.SetTypeface(fontFace, TypefaceStyle.Normal);
-            largeTextView?.SetTypeface(fontFaceSelected, TypefaceStyle.Normal);
-        }
-#pragma warning restore XAOBS001
+        var renderer = new NaluShellItemRenderer(this);
+        ((IShellItemRenderer)renderer).ShellItem = shellItem;
+        return renderer;
     }
 }
--- Source/Nalu.Maui.Navigation/Platforms/iOS/NaluShellRenderer.cs
+++ Source/Nalu.Maui.Navigation/Platforms/iOS/NaluShellRenderer.cs
@@ -1,9 +1,5 @@
-using System.Collections.Specialized;
-using System.Linq;
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
-using Microsoft.Maui.Platform;
-using UIKit;
 
 namespace Nalu;
 
@@ -18,77 +14,5 @@ internal class NaluShellRenderer : ShellRenderer
         renderer.UpdateTabBarView();
 
         return renderer;
-    }
-}
-
-internal class NaluShellItemRenderer : ShellItemRenderer
-{
-    private const nint _mauiTabBarTag = 0x63D2AF;
-    private readonly IShellContext _shellContext;
-    private UIView? _platformTabBar;
-    private View? _crossPlatformTabBar;
-
-    public NaluShellItemRenderer(IShellContext shellContext)
-        : base(shellContext)
-    {
-        _shellContext = shellContext;
-    }
-
-    protected override void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-    {
-        base.OnItemsCollectionChanged(sender, e);
-        UpdateTabBarView();
-    }
-
-    public void UpdateTabBarView()
-    {
-        // Get the TabBarView from the ShellItem
-        var tabBarView = NaluShell.GetTabBarView(ShellItem);
-
-        if (tabBarView == null)
-        {
-            // Remove custom view if it was previously added
-            _crossPlatformTabBar?.DisconnectHandlers();
-            _platformTabBar?.RemoveFromSuperview();
-            _platformTabBar = null;
-            _crossPlatformTabBar = null;
-
-            // Show native tab bar items (in case they were hidden)
-            HideNativeTabBarItems(false);
-        }
-        else if (_crossPlatformTabBar != tabBarView)
-        {
-            _crossPlatformTabBar?.DisconnectHandlers();
-            _platformTabBar?.RemoveFromSuperview();
-
-            var mauiContext = _shellContext.Shell.Handler?.MauiContext ?? throw new NullReferenceException("MauiContext is null");
-            var platformView = tabBarView.ToPlatform(mauiContext);
-            platformView.Tag = _mauiTabBarTag;
-            platformView.TranslatesAutoresizingMaskIntoConstraints = false;
-            _platformTabBar = platformView;
-            _crossPlatformTabBar = tabBarView;
-            TabBar.AddSubview(_platformTabBar);
-            NSLayoutConstraint.ActivateConstraints(
-                [
-                    platformView.LeadingAnchor.ConstraintEqualTo(TabBar.LeadingAnchor),
-                    platformView.TrailingAnchor.ConstraintEqualTo(TabBar.TrailingAnchor),
-                    platformView.BottomAnchor.ConstraintEqualTo(TabBar.BottomAnchor),
-                    platformView.TopAnchor.ConstraintEqualTo(TabBar.TopAnchor)
-                ]
-            );
-            HideNativeTabBarItems(true);
-        }
-    }
-
-    private void HideNativeTabBarItems(bool hidden)
-    {
-        // Show all subviews that are not our custom MAUI bar
-        foreach (var subview in TabBar.Subviews)
-        {
-            if (subview.Tag != _mauiTabBarTag)
-            {
-                subview.Hidden = hidden;
-            }
-        }
     }
 }
